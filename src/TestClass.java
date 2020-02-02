7d6c291716ef34ed8b8529ac2951a1a75293108b
Stydent <urda1975@mail.ru>
2020-02-02 20:22:19 +0300

    MyTestStart

---
+++ src/TestClass.java
@@ -0,0 +1,14 @@
+public class TestClass {
+
+
+    static double fractional(double num){
+
+        return num % 1;
+
+    }
+    public static void main(String[] args){
+        System.out.println("текст первый");
+
+        System.out.println(fractional(1.53));
+    }
+}
