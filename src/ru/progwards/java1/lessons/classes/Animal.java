352dd55bd726d90159f529028925f9a6dfc7c1af
Stydent <urda1975@mail.ru>
2020-02-07 19:20:57 +0300

    Animal2

--- src/ru/progwards/java1/lessons/classes/Animal.java
+++ src/ru/progwards/java1/lessons/classes/Animal.java
@@ -34,7 +34,11 @@ public class Animal {
 
     @Override
     public String toString(){
-        return "I am " + getFoodKind() + ", eat " + getKind() + " " + calculateFoodWeight() ;
+        return "I am " + getKind() + ", eat " + getFoodKind() + " " + calculateFoodWeight() ;
+    }
+
+    public String toStringFull(){
+        return "I am " + getKind() + ", eat " + getFoodKind() + " " + calculateFoodWeight() ;
     }
 
     public static void main(String[] args){
@@ -42,20 +46,24 @@ public class Animal {
         System.out.println(animal.getKind());
         System.out.println(animal.getFoodKind());
         System.out.println(animal.toString());
+        System.out.println(animal.toStringFull());
 
         Cow cow = new Cow(68);
         System.out.println(cow.getKind());
         System.out.println(cow.getFoodKind());
         System.out.println(cow.toString());
+        System.out.println(cow.toStringFull());
 
         Hamster hamster = new Hamster(68);
         System.out.println(hamster.getKind());
         System.out.println(hamster.getFoodKind());
         System.out.println(hamster.toString());
+        System.out.println(hamster.toStringFull());
 
         Duck duck = new Duck(68);
         System.out.println(duck.getKind());
         System.out.println(duck.getFoodKind());
         System.out.println(duck.toString());
+        System.out.println(duck.toStringFull());
     }
 }
