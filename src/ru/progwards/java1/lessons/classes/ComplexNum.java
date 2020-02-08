819db28ba17246baf1fac4be31e3bc088c029a6f
Stydent <urda1975@mail.ru>
2020-02-08 16:32:46 +0300

    ComplexNum

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -30,26 +30,28 @@ public class ComplexNum {
         num = new ComplexNum(this.a, this.b);
         return num;
     }
+
     public  ComplexNum  mul(ComplexNum num){
-        this.a = a * num.a - b * num.b;
-        this.b = b * num.a + a * num.b ;
-        num = new ComplexNum(this.a, this.b);
+        //(a + b) * (num.a + num.b)
+        int a1 = a * num.a - b * num.b;
+        int b1 = b * num.a + a * num.b;
+        num = new ComplexNum(a1, b1);
         return num;
     }
     public  ComplexNum   div(ComplexNum num){
-        this.a = (a * num.a + b * num.b) / (num.a * num.a + num.b * num.b);
-        this.b = ((b * num.a - a * num.b) / (num.a * num.a + num.b * num.b)) ;
-        num = new ComplexNum(this.a, this.b);
+        int a2 = (a * num.a + b * num.b) / (num.a * num.a + num.b * num.b);
+        int b2 = ((b * num.a - a * num.b) / (num.a * num.a + num.b * num.b)) ;
+        num = new ComplexNum(a2, b2);
         return num;
     }
 
 
     public static void main(String[] args) {
-        ComplexNum complexNum = new ComplexNum(1, 56);
-        ComplexNum complexNum2 = new ComplexNum(4, 58);
+        ComplexNum complexNum = new ComplexNum(1000, 1000);
+        ComplexNum complexNum2 = new ComplexNum(100, 100);
         System.out.println(complexNum.add(complexNum2));
-        System.out.println(complexNum.add(complexNum2));
-        System.out.println(complexNum.sub(complexNum2));
+//        System.out.println(complexNum.add(complexNum2));
+       System.out.println(complexNum.sub(complexNum2));
         System.out.println(complexNum.mul(complexNum2));
         System.out.println(complexNum.div(complexNum2));
     }
