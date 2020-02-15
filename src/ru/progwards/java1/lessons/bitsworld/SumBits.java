b61fe0af7fcf45cd201fc7bbd3c791e9042c5786
Stydent <urda1975@mail.ru>
2020-02-15 19:35:40 +0300

    Binary

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -4,17 +4,24 @@ public class SumBits {
 
     public static int sumBits(byte value){
         int sum = 0;
-        for (int i=0; 1<=value; i++){
+
+        for (int i=0; i < 8; i++){
             sum = sum + (value & 1);
            value >>= 1;
+
+           if(value==0)
+               break;
+
+
         }
+
         return sum;
     }
 
     public static void main(String[] args) {
-        byte value = 0b00001101111;
+        byte value = -128;
         System.out.println(sumBits(value));
         System.out.println();
-        sumBits(value);
+        //sumBits(value);
     }
 }
