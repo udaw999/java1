243fe88e52c47055bf70121bbe55465186f35efa
Stydent <urda1975@mail.ru>
2020-03-02 18:45:30 +0300

    AbsInteger 3.3

--- src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -1,23 +1,34 @@
 package ru.progwards.java1.lessons.bigints;
 
 import java.math.BigInteger;
+import java.sql.Array;
+import java.util.Arrays;
 
 public class ArrayInteger {
 
     byte[] digits;
     int n;
+    boolean bool = true;
     //размер массива
-    public ArrayInteger(int n){
+    public ArrayInteger(int n) {
 
-        this.n =n;
+        this.n = n;
 
     }
 
-    public void fromInt(BigInteger value){
+    //(уложить BigInteger во внутренний массив)
+    //Например, число 159 должно занять 3 ячейки массива digits[0] = 9; digits[1] = 5; digits[2] = 1;
+    public void fromInt(BigInteger value) {
         digits = new byte[n];
-        for (int i=0; value.compareTo(BigInteger.ZERO) != 0; i++) {
 
-            System.out.println(value);
+        for (int i = 0; value.compareTo(BigInteger.ZERO) != 0; i++) {
+            if (i>n-1){
+                //проверка При переполнении массива вернуть false, при этом само число сбросить в 0
+                Arrays.fill(digits,(byte)0);
+                bool = false;
+                break;
+            }
+
             //получаем остаток деления на 10 и приводим к строке
             String str = "" + value.mod(BigInteger.TEN);
             //помещаем в  массив преобразовав число из String в Byte
@@ -29,31 +40,55 @@ public class ArrayInteger {
 
     }
 
-    public static void main(String[] args) {
-       // byte[] digits = {0,0,0,0,0,0};
-        int z = 6;
-        ArrayInteger arrayInteger = new ArrayInteger(6);
-        BigInteger value = BigInteger.valueOf(123457);
-   /*     int val = 123456;
-        for (int i=0; val > 0; i++) {
-//            System.out.println(value);
-//            value = value.divide(BigInteger.TEN);
-            System.out.println(val);
-            int n = val % 10;
-            val = val / 10;
-            digits[i] = (byte)n;
-            System.out.println(n);
+    public BigInteger toInt() {
 
+        String str = "";
+        //собираем число в строку из массива
+        for (int i = 0; i < n; i++) {
+            str = digits[i] + str;
         }
-        for (byte b : digits) {
-            System.out.print(b + ", ");
-        }*/
 
-
-        //System.out.println(arrayInteger.fromInt(value));
-
-
-
-
+        return BigInteger.valueOf(Integer.parseInt(str));
     }
+    //сложить 2 числа, не используя BigInteger,
+    //собираем числа в строке вытаскивая по цифре из массива
+    public boolean add(ArrayInteger num) {
+        ArrayInteger arrayInteger1 = (ArrayInteger) num;
+
+
+        String num1 = "";
+        String num2 = "";
+
+        for (int i=0; i < digits.length; i++){
+
+            num1 = digits[i] + num1;
+        }
+        for (int i=0; i < arrayInteger1.digits.length; i++){
+
+            num2 = arrayInteger1.digits[i] + num2;
+        }
+        //складываем числа переведя из строки в Integer
+        long sum = Integer.parseInt(num1) + Integer.parseInt(num2);
+        //отправляем число в массив
+        fromInt(BigInteger.valueOf(sum));
+
+        return bool;
+    }
+
+    public static void main(String[] args) {
+        // byte[] digits = {0,0,0,0,0,0};
+        int z = 6;
+        ArrayInteger arrayInteger = new ArrayInteger(8);
+        BigInteger value = BigInteger.valueOf(123);
+        arrayInteger.fromInt(value);
+
+        ArrayInteger arrayInteger2 = new ArrayInteger(8);
+        BigInteger value2 = BigInteger.valueOf(56789557);
+        arrayInteger2.fromInt(value2);
+
+
+        System.out.println(arrayInteger.add(arrayInteger2));
+        System.out.println(value.add(value2));
+    }
+
 }
