c19f52a22e17e9bc63edb517d8f09420d1909c2c
Stydent <urda1975@mail.ru>
2020-03-12 21:12:12 +0300

    Bit 2

---
+++ src/ru/progwards/java1/lessons/register1/Counter.java
@@ -0,0 +1,10 @@
+package ru.progwards.java1.lessons.register1;
+
+public class Counter {
+    public static void inc(ByteRegister value){
+
+    }
+    public static void dec(ByteRegister value){
+
+    }
+}
---
+++ src/ru/progwards/java1/lessons/register1/ShiftRegister.java
@@ -0,0 +1,8 @@
+package ru.progwards.java1.lessons.register1;
+
+public class ShiftRegister {
+
+    public static void left(ByteRegister value){}
+
+    public static void right(ByteRegister value){}
+}
