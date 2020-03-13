75898a6e7f72d709cb8c3988612bcb82971fe812
Stydent <urda1975@mail.ru>
2020-03-13 20:59:36 +0300

    Bit 2.3

--- src/ru/progwards/java1/lessons/register1/Counter.java
+++ src/ru/progwards/java1/lessons/register1/Counter.java
@@ -2,13 +2,17 @@ package ru.progwards.java1.lessons.register1;
 
 public class Counter {
     public static void inc(ByteRegister value){
+        System.out.println(value.toDecString());
         System.out.println(value);
         int valueByte = Integer.parseInt(value.toDecString());
         System.out.println(valueByte);
         valueByte++;
         value.toDecString();
-        System.out.println(value.toDecString());
+        new ByteRegister( (byte) valueByte);
+        System.out.println(new ByteRegister( (byte) valueByte).toDecString());
+
         System.out.println(valueByte);
+
     }
     public static void dec(ByteRegister value){
         int valueByte = Integer.parseInt(value.toString());
@@ -18,6 +22,6 @@ public class Counter {
 
     public static void main(String[] args) {
 
-        inc(new ByteRegister((byte) 0));
+        inc(new ByteRegister((byte) 100));
     }
 }
