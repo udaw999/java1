45628ee8bb52757dc5208a09d04b3a818bd310fa
Stydent <urda1975@mail.ru>
2020-03-15 19:35:07 +0300

    Coder 3.7

--- src/ru/progwards/java1/lessons/register1/ShiftRegister.java
+++ src/ru/progwards/java1/lessons/register1/ShiftRegister.java
@@ -3,30 +3,33 @@ package ru.progwards.java1.lessons.register1;
 public class ShiftRegister {
 
     public static void left(ByteRegister value){
+        //System.out.println(value);
 
-        value.value[7]=0;
         for (int i = 0; i < 7; i++) {
 
             value.value[i]=value.value[i+1];
 
         }
-
+        value.value[7]=0;
+       // for (int values : value.value) {           System.out.print("," + values);       }
     }
 
     public static void right(ByteRegister value){
-        //System.out.println(value);
+
         for (int i = 0; i < 7; i++) {
 
             value.value[value.value.length-i-1]=value.value[value.value.length-i-2];
 
         }
         value.value[0] = 0;
-        //for (int values : value.value) {           System.out.print("," + values);       }
+
     }
 
     public static void main(String[] args) {
-        ByteRegister b = new ByteRegister((byte) 100);
+        ByteRegister b = new ByteRegister((byte) 1);
 
-        right(b);
+        left(b);
+        System.out.println();
+        System.out.println(b.toDecString());
     }
 }
