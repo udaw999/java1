08da15462c7740c17d523088ebec53027a20e8a5
Stydent <urda1975@mail.ru>
2020-03-23 13:05:38 +0300

    collections 1 -2

--- src/ru/progwards/java1/lessons/collections/Creator.java
+++ src/ru/progwards/java1/lessons/collections/Creator.java
@@ -18,8 +18,8 @@ public class Creator {
 
     public static Collection<Integer> fillOdd(int n){
         List<Integer> arrayList = new ArrayList();
-        int m = n*3*2+1;
-        for (int i=0; i<n*3; i++){
+        int m = n*2+1;
+        for (int i=0; i<n; i++){
             m -= 2;
             arrayList.add(m);
         }
@@ -28,10 +28,17 @@ public class Creator {
 
     public static Collection<Integer> fill3(int n){
         List<Integer> arrayList = new ArrayList();
-        int m = 0;
-        for (int i=0; i<n*3; i++){
-            m = Integer.parseInt(i + "" + (i*i) + "" + (i*i*i));
-            arrayList.add(m);
+
+        int z = 0;
+        for (int i=0; i<n; i++){
+
+            List<Integer> arrayList3 = new ArrayList();
+                arrayList3.add(0,i*3);
+                arrayList3.add(1,i*i*9);
+                arrayList3.add(2,i*i*i*27);
+
+         arrayList.addAll(arrayList3);
+
         }
         return arrayList;
     }
@@ -40,8 +47,8 @@ public class Creator {
         System.out.println("четные по возрастанию");
         System.out.println(fillEven(26));
         System.out.println("нечетных убывающих чисел");
-        System.out.println(fillOdd(10));
+        System.out.println(fillOdd(8));
         System.out.println("тройками чисел");
-        System.out.println(fill3(2));
+        System.out.println(fill3(5));
     }
 }
