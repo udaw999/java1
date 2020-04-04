0e79349d271079976610fb2f7c2013c6ca01a666
Stydent <urda1975@mail.ru>
2020-04-04 19:26:43 +0300

    IntRegister 1-2

--- src/ru/progwards/java1/lessons/register2/IntRegister.java
+++ src/ru/progwards/java1/lessons/register2/IntRegister.java
@@ -26,19 +26,35 @@ public class IntRegister extends Register {
 
     public String toDecString(){
         int valueDes = 0;
-        for (int i = 0; i < 32; i++) {
-            // System.out.println(valueDes + " = " +  this.value[i] + " умножить на 2 в степени " + (this.value.length - i - 1 ));
-            valueDes += this.value[i] * Math.pow(2,this.value.length - i -1);
+        if(value[0] == 0){
+            for (int i = 0; i < 32; i++) {
+                // System.out.println(valueDes + " = " +  this.value[i] + " умножить на 2 в степени " + (this.value.length - i - 1 ));
+                valueDes += this.value[i] * Math.pow(2,this.value.length - i -1);
 
+            }
+        } else {
+            for (int i = 0; i < 32; i++) {
+                // System.out.println(valueDes + " = " +  this.value[i] + " умножить на 2 в степени " + (this.value.length - i - 1 ));
+                valueDes += (this.value[i] ^ 1) * Math.pow(2, this.value.length - i - 1);
+            }
+            valueDes += 1;
+            valueDes *= -1;
         }
+
         return String.valueOf(valueDes);
     }
 
     public static void main(String[] args) {
-        int value = 157;
+        int value = -451502843;
         IntRegister intRegister = new IntRegister(value);
 
         System.out.println(intRegister);
-        System.out.println(intRegister.toDecString());
+        System.out.println(intRegister.toString());
+        System.out.println(intRegister.toDecString());//значением параметра = -451502843 ожидался: -451502843
+
+        for (byte values : intRegister.value) {           System.out.print(", " + values);       }
     }
 }
+/*
+00011010111010010110001011111011  451502843
+11100101000101101001110100000101  -451502843 */
