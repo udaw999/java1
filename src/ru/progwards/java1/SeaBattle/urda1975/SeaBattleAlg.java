731ece8eb7979b0193106a47ffeb0662f3d9ba3e
Stydent <urda1975@mail.ru>
2020-04-12 10:49:24 +0300

    морской бой3

--- src/ru/progwards/java1/SeaBattle/urda1975/SeaBattleAlg.java
+++ src/ru/progwards/java1/SeaBattle/urda1975/SeaBattleAlg.java
@@ -30,7 +30,7 @@ public class SeaBattleAlg {
     //         8|X|.|.|.|.|.|.|X|.|.|
     //         9|X|.|.|.|X|.|.|.|.|.|
 
-    public void battleAlgorithm(SeaBattle seaBattle) {
+  /*  public void battleAlgorithm(SeaBattle seaBattle) {
         // пример алгоритма:
         // стрельба по всем квадратам поля полным перебором
         for (int y = 0; y < seaBattle.getSizeX(); y++) {
@@ -38,6 +38,74 @@ public class SeaBattleAlg {
                 SeaBattle.FireResult fireResult = seaBattle.fire(x, y);
             }
         }
+    }*/
+    //алгоритм вывода
+    public void battleAlgorithm(SeaBattle seaBattle) {
+        // пример алгоритма:
+        // стрельба по всем квадратам поля полным перебором
+        //init(seaBattle);//заполняемый массив результатами прострела
+
+//        field[3][3] = 'X';
+//        field[3][4] = 'X';
+//        field[3][5] = 'X';
+//        field[3][6] = 'X';
+//        field[3][7] = 'X';
+        int n = 0;//для перебора
+        int z = 1;//для перебора
+        int y = 0;
+        int x = 0;
+        while ( y < seaBattle.getSizeY() ) {
+            x = 3 - y - n;
+            if( x < 0 && x >= -4){
+                x = x + 4;
+            }
+            if(x < -4 && x >= -8){
+                x = x + 8;
+            }
+            if(x < -8 && x >= -12){
+                x = x + 12;
+            }
+            while (x < seaBattle.getSizeX()) {
+
+                //if (field[x][y] == ' ') {//усли ячейка в моем поле пуста то стреляем
+
+
+                    FireResult fireResult = seaBattle.fire(x, y);//выстрел
+                    //System.out.println("DESTROYED - " + FireResult.DESTROYED);
+                    //markFire(x, y, fireResult);//визуализация-- заполняет мое поле результатами стрельбы
+                    if (fireResult != FireResult.MISS) {//если не промахнулись
+
+                        //noMiss(x, y, fireResult);// обрабатываем обводку, поиск палубы еще
+                        // если ранен ++ считаем выстрелы попаданий
+
+                    }
+               // }
+
+                x = x + 4;//для перебора
+            }
+
+
+            if(y == 9 && z == 1 ) {
+                y = 0;
+                n = 2;
+                z++;
+            } else  if (y == 9 && z == 2 ){
+                y = 0;
+                n = 3;
+                z++;
+            } else  if (y == 9 && z == 3 ){
+                y = 0;
+                n = 1;
+                z++;
+            } else  {
+                y++;
+            }
+
+
+        }
+
+
+
     }
 
     // функция для отладки
