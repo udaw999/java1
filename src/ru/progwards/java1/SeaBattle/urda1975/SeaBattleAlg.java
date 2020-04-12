3143b606c1658da1ef1c16f467ecff79c6da6e0a
Stydent <urda1975@mail.ru>
2020-04-12 13:18:42 +0300

    морской бой18

--- src/ru/progwards/java1/SeaBattle/urda1975/SeaBattleAlg.java
+++ src/ru/progwards/java1/SeaBattle/urda1975/SeaBattleAlg.java
@@ -276,8 +276,9 @@ public class SeaBattleAlg {
             }
             while (x < seaBattle.getSizeX()) {
 
-
-                System.out.println("hits - " + hits);
+                if (hits == 20)
+                    break;
+                //System.out.println("hits - " + hits);
                 if (field[x][y] == ' ') {//усли ячейка в моем поле пуста то стреляем
 
 
