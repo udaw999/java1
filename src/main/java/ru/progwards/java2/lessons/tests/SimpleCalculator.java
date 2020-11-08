7e34de5d2aa11e9f7bd04ec8636c0fb1ca896416
Stydent <urda1975@mail.ru>
2020-11-08 14:47:01 +0300

    test

--- src/main/java/ru/progwards/java2/lessons/tests/SimpleCalculator.java
+++ src/main/java/ru/progwards/java2/lessons/tests/SimpleCalculator.java
@@ -11,16 +11,31 @@ public class SimpleCalculator implements ISimpleCalculator {
 
     @Override
     public int diff(int val1, int val2) {
+        long result = (long) val1 - val2;
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE)
+            throw new ArithmeticException();
         return val1 - val2;
     }
 
     @Override
     public int mult(int val1, int val2) {
+        long result = (long) val1 * val2;
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE)
+            throw new ArithmeticException();
         return val1 * val2;
     }
 
     @Override
     public int div(int val1, int val2) {
+        long result = (long) val1 / val2;
+        if (result > Integer.MAX_VALUE || result < Integer.MIN_VALUE || val2 == 0)
+            throw new ArithmeticException();
         return val1 / val2;
+    }
+
+    public static void main(String[] args) {
+        SimpleCalculator cal = new SimpleCalculator();
+        System.out.println(cal.div(0 , 40));
+
     }
 }
--- src/test/java/ru/progwards/java2/lessons/tests/test/calc/SimpleCalculatorTest.java
+++ src/test/java/ru/progwards/java2/lessons/tests/test/calc/SimpleCalculatorTest.java
@@ -1,43 +1,141 @@
 package ru.progwards.java2.lessons.tests.test.calc;
 
 import org.junit.*;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import ru.progwards.java2.lessons.tests.SimpleCalculator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
+
+@RunWith(Enclosed.class)
 public class SimpleCalculatorTest {
-    static SimpleCalculator calculator;
+
+    static SimpleCalculator calc;
 
     @BeforeClass
-    public static void init(){
-        calculator = new SimpleCalculator();
+    public static void init(){calc = new SimpleCalculator();}
+
+    @RunWith(Parameterized.class)
+    public static class SumSimpleCalculatorTest {
+        @Parameterized.Parameter(0)
+        public int val1;
+        @Parameterized.Parameter(1)
+        public int val2;
+        @Parameterized.Parameter(2)
+        public int expected;
+
+        @Parameterized.Parameters(name = "Тест {index}: ({0}) + ({1}) = {2}")
+        public static Iterable<Object[]> dataForTest(){
+            return Arrays.asList(new Object[][]{
+                    {0, 0, 0},
+                    {5, 0, 5},
+                    {-5, -5, -10},
+                    {34, 55, 89},
+                    {-34, -55, -89}
+            });
+
+        }
+        @Test
+        public void testWithParams(){assertEquals(expected, calc.sum(val1,val2));}
+
+
+
+
+    }
+    public static class SumExceptionTest{
+        @Test(expected = ArithmeticException.class)
+        public void SumWithException(){calc.sum(Integer.MAX_VALUE, 1);}
     }
 
-    @Test
-    public void testSum(){
+    @RunWith(Parameterized.class)
+    public static class DiffSimpleCalculatorTest {
+        @Parameterized.Parameter(0)
+        public int val1;
+        @Parameterized.Parameter(1)
+        public int val2;
+        @Parameterized.Parameter(2)
+        public int expected;
 
-        assertEquals(calculator.sum(0,0),0);
-        assertEquals(calculator.sum(2,3),5);
-        assertEquals(calculator.sum(-2,-3),-5);
-        assertEquals(calculator.sum(-5,5),0);
+        @Parameterized.Parameters(name = "Тест {index}: ({0}) - ({1}) = {2}")
+        public static Iterable<Object[]> dataForTest(){
+            return Arrays.asList(new Object[][]{
+                    {0, 0, 0},
+                    {4, 0, 4},
+                    {-5, -5, 0},
+                    {34, 55, -21},
+                    {-34, -55, 21}
+            });
 
+        }
+        @Test
+        public void testWithParams(){assertEquals(expected, calc.diff(val1,val2));}
+    }
+    public static class DiffExceptionTest{
+        @Test(expected = ArithmeticException.class)
+        public void DiffWithException(){calc.diff(Integer.MAX_VALUE, -1);}
     }
 
-    @Test(expected = ArithmeticException.class)
-    public void testException(){
-        assertEquals(calculator.sum(Integer.MAX_VALUE,1),0);
+    @RunWith(Parameterized.class)
+    public static class MultSimpleCalculatorTest {
+        @Parameterized.Parameter(0)
+        public int val1;
+        @Parameterized.Parameter(1)
+        public int val2;
+        @Parameterized.Parameter(2)
+        public int expected;
 
+        @Parameterized.Parameters(name = "Тест {index}: ({0}) * ({1}) = {2}")
+        public static Iterable<Object[]> dataForTest(){
+            return Arrays.asList(new Object[][]{
+                    {0, 0, 0},
+                    {5, 0, 0},
+                    {-5, -5, 25},
+                    {-4, 5, -20},
+                    {-34, -55, 1870}
+            });
+
+        }
+        @Test
+        public void testWithParams(){assertEquals(expected, calc.mult(val1,val2));}
+    }
+    public static class MultExceptionTest{
+        @Test(expected = ArithmeticException.class)
+        public void MultWithException(){calc.mult(Integer.MAX_VALUE, 2);}
     }
 
-    @Test
-    public void testDiff(){
-        assertEquals(calculator.diff(0,0),0);
+    @RunWith(Parameterized.class)
+    public static class DivSimpleCalculatorTest {
+        @Parameterized.Parameter(0)
+        public int val1;
+        @Parameterized.Parameter(1)
+        public int val2;
+        @Parameterized.Parameter(2)
+        public int expected;
 
+        @Parameterized.Parameters(name = "Тест {index}: ({0}) / ({1}) = {2}")
+        public static Iterable<Object[]> dataForTest(){
+            return Arrays.asList(new Object[][]{
+                    {4, 4, 1},
+                    {0, 5, 0},
+                    {-25, -5, 5},
+                    {-4, 5, 0},
+                    {1800, -55, -32}
+            });
+
+        }
+        @Test
+        public void testWithParams(){assertEquals(expected, calc.div(val1,val2));}
+    }
+    public static class DivExceptionTest{
+        @Test(expected = ArithmeticException.class)
+        public void DivWithException(){calc.div(Integer.MAX_VALUE, 0);}
     }
 
     @AfterClass
-    public static void destroy(){
-        calculator = null;
-    }
-
+    public static void end(){calc = null;}
 }
